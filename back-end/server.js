42ddc3c303dbb9fd03b68e9afadccde1284c7760
nandinivenkateshan <nandinivenkatesan809@gmail.com>
2019-12-24 21:04:06 +0530

    Modified server.js

--- back-end/server.js
+++ back-end/server.js
@@ -18,7 +18,7 @@ app.use(function (req, res, next) {
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('../front-end/dist'))
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'front-end', 'dist', 'index.html'))
+    res.sendFile(path.resolve(__dirname, '../front-end', 'dist', 'index.html'))
   })
 }
 
